1a97944a1a5fcc2906eaed8b4ac44ff576bae448
Ign0reLee <dlwpdud@gmail.com>
2020-11-14 00:37:49 +0900

    Update Random Crop And Reszing

--- net/Func.py
+++ net/Func.py
@@ -26,12 +26,32 @@ def load_image(path_to_img):
 
     return img
 
+def load_batch_image(path_to_img, min_dim = 512, crop_size = 256):
+
+    #Image Load
+    img = cv2.imread(path_to_img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    #Image Resize
+    shape = np.shape(img)[:-1]
+    small_dim = min(shape)
+    scale = min_dim / small_dim
+    shape = tuple((np.array(shape) * scale).astype(np.int32))
+    img = cv2.resize(img, dsize=shape)
+
+    #Random Shape
+    init_w = np.random.randint(shape[0] - crop_size)
+    init_h = np.random.randint(shape[1] - crop_size)
+    img = img[init_h: init_h+crop_size, init_w: init_w+crop_size, :]
+
+    return img
+
 def make_image_minbatch(path_to_img):
 
     batches = []
 
     for path  in path_to_img:
-        batches.append(load_image(path))
+        batches.append(load_batch_image(path))
         
     return preprocess_input(np.array(batches))
 
